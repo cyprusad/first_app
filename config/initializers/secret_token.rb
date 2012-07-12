b583617d1dfb665303ab4c147c17ce1da80c7237
Sai Warang <cyprusad@gmail.com>
2012-07-12 16:15:28 -0700

    Initial commit of skeletal scaffolding app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6247b48a586b28d06584db26c4f784cda908504c6ca50b1b8761f2e98fdeebb8dcfeeb5b338377108c3ebe1310825270fa9c8e5312571aaf382ddc90102c8585'
